b35172f498b7cf97851b7163a2d314994fd6e235
Dwayne <d70509@yahoo.com>
2021-08-12 22:13:30 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '524971375391249',
+        clientSecret: '62b99848099fc8fceda320a4bf86483eRese'
+    }
 }
